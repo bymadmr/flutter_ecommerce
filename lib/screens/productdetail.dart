a21ec7573bd3bfa16c4d8a1cb932f89d7fc7cef3
bymadmr@outlook.com <bymadmr@outlook.com>
2021-04-24 03:53:23 +0300

    Complete ProductDetail page

--- lib/screens/productdetail.dart
+++ lib/screens/productdetail.dart
@@ -28,17 +28,46 @@ class _ProductDetailState extends State with TickerProviderStateMixin {
         ),
       ),
       body: _buildProductDetails(context),
+      bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
   _buildProductDetails(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return ListView(
       children: <Widget>[
         Container(
           padding: EdgeInsets.all(5),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [_buildProductImages()],
+            children: [
+              _buildProductImages(),
+              _buildProductTitle(),
+              SizedBox(
+                height: 12,
+              ),
+              _buildProductPrice(),
+              SizedBox(
+                height: 12,
+              ),
+              _buildDivider(size),
+              SizedBox(
+                height: 12,
+              ),
+              _buildFutherInfo(),
+              SizedBox(
+                height: 12,
+              ),
+              _buildDivider(size),
+              SizedBox(
+                height: 12,
+              ),
+              _buildSizeArea(),
+              SizedBox(
+                height: 12,
+              ),
+              _buildInfo()
+            ],
           ),
         )
       ],
@@ -81,5 +110,218 @@ class _ProductDetailState extends State with TickerProviderStateMixin {
         ),
       ),
     );
+  }
+
+  _buildProductTitle() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      child: Center(
+        child: Text(
+          "Özel Marka Kazak",
+          style: TextStyle(fontSize: 16, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  _buildProductPrice() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      child: Row(
+        children: [
+          Text(
+            "\$ 100",
+            style: TextStyle(fontSize: 16, color: Colors.black),
+          ),
+          SizedBox(
+            width: 8,
+          ),
+          Text(
+            "\$ 200",
+            style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+                decoration: TextDecoration.lineThrough),
+          ),
+          SizedBox(
+            width: 8,
+          ),
+          Text(
+            "\50% indirim",
+            style: TextStyle(fontSize: 12, color: Colors.blue),
+          ),
+          SizedBox(
+            width: 8,
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildDivider(Size screenSize) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.grey,
+          width: screenSize.width,
+          height: 0.25,
+        )
+      ],
+    );
+  }
+
+  _buildFutherInfo() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      child: Row(
+        children: [
+          Icon(Icons.local_offer, color: Colors.grey),
+          SizedBox(
+            width: 12,
+          ),
+          Text(
+            "Daha fazla bilgi için tıklayınız",
+            style: TextStyle(color: Colors.grey),
+          )
+        ],
+      ),
+    );
+  }
+
+  _buildSizeArea() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(
+                Icons.straighten,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Text(
+                "Beden : M",
+                style: TextStyle(color: Colors.grey),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                "Beden Tablosu",
+                style: TextStyle(fontSize: 14, color: Colors.blue),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  _buildInfo() {
+    TabController tabController = TabController(length: 2, vsync: this);
+    return Container(
+      child: Column(
+        children: [
+          TabBar(
+            controller: tabController,
+            tabs: [
+              Tab(
+                child: Text(
+                  "Ürün Bilgisi",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Yıkama Bilgisi",
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+            ],
+          ),
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+              height: 40,
+              child: TabBarView(
+                controller: tabController,
+                children: [
+                  Text(
+                    "% 60 Polyester. Kaliteli bir ürün.",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    "30 derece renkli yıkama.",
+                    style: TextStyle(color: Colors.black),
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+
+  _buildBottomNavigationBar() {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 1,
+              child: RaisedButton(
+                onPressed: () {},
+                color: Colors.grey,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.list,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      "İstek",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 2,
+              child: RaisedButton(
+                onPressed: () {},
+                color: Colors.greenAccent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.card_travel,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      "Sepete Ekle",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
